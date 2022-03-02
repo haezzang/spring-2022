8deb84d196b2911501a4dbe909d9c40935aefdae
haezzang <s2036@e-mirim.hs.kr>
2022-03-02 11:52:11 +0900

    first commit

---
+++ JavaBasic/src/kr/hs/study/HelloWorldEn.java
@@ -0,0 +1,18 @@
+package kr.hs.study;
+
+public class HelloWorldEn {
+
+	
+	static void sayHello(){
+		System.out.println("Hello");
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		sayHello();
+		
+
+	}
+
+}
